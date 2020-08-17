f2ed8d888eaad0484e55a5e697215aa61c4cb4ca
guobgf <guobgf@yonyou.com>
2020-08-17 10:08:02 +0800

    init

---
+++ template/tmp.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const fs = require('fs');
+const basePath = path.resolve(__dirname, '../src');
+const dirName = process.argv[2];
+
+if (!dirName) {
+    console.log('请输入组件名称');
+    return;
+}
+
+if (fs.existsSync(`${basePath}/component/${dirName}`)) {
+    console.log('该组件已存在，请检查');
+    return;
+}
+
+const vueTmp = `<template>
+<div class="${dirName}-wrap">{{pageName}}</div>
+</template>
+
+<script src="./${dirName}.js"></script>
+
+<style lang="less">
+@import "./${dirName}.less";
+</style>`;
+
+const jsTmp = `export default {
+    name: "page1",
+    data: function (): Object {
+        return {
+            pageName: "page1",
+        };
+    }
+};`;
+
+const lessTmp = `.${dirName}-wrap{
+    color: aqua;
+}`;
+
+fs.mkdirSync(`${basePath}/component/${dirName}`);
+process.chdir(`${basePath}/component/${dirName}`);
+fs.writeFileSync(`${dirName}.vue`, vueTmp);
+fs.writeFileSync(`${dirName}.js`, jsTmp);
+fs.writeFileSync(`${dirName}.less`, lessTmp);
+
+process.exit(0);
---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const path = require("path");
+const VueLoaderPlugin = require("vue-loader/lib/plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+    mode: "development",
+    entry: "./src/main.ts",
+    output: {
+        filename: "bundle.[chunkhash].js",
+        path: path.join(__dirname, "dist"),
+    },
+    resolve: {
+        alias: {
+            vue$: "vue/dist/vue.esm.js",
+            "@": path.join(__dirname, "..", "src"),
+        },
+        extensions: [".vue", ".js", ".jsx"],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: ["babel-loader"],
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.vue$/,
+                use: ["vue-loader"],
+            },
+            {
+                test: /\.(js|vue)$/,
+                loader: "eslint-loader",
+                enforce: "pre",
+                //指定检查的目录
+                include: [path.resolve(__dirname, 'src')]
+            },
+            {
+                test: /\.less$/,
+                loader: 'style-loader!css-loader!less-loader'
+            },
+        ],
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new VueLoaderPlugin(),
+        new HtmlWebpackPlugin({
+            template: "htmlTmp/index.html",
+        }),
+    ],
+    devServer: {
+        contentBase: path.join(__dirname, "dist"), // dist目录开启服务器
+        compress: true, // 是否使用gzip压缩
+        port: 9000, // 端口号
+        open: true, // 自动打开网页
+    },
+};
